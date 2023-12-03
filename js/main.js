dd40b14063072fa4c5db05d17af76316f8fa142e
SergoLondaridze <sergo.londaridze@gmail.com>
2023-12-04 01:14:11 +0400

    add event listener for form

---
+++ js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList=document.querySelector('#tasksList');
+
+form.addEventListener('submit', addTask);
+
+function addTask(event) {
+
+    event.preventDefault();
+
+    const taskText = taskInput.value;
+
+    const taskHTML = `
+        <li class="list-group-item d-flex justify-content-between task-item">
+            <span class="task-title">${taskText}</span>
+            <div class="task-item__buttons">
+                <button type="button" data-action="done" class="btn-action">
+                    <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                </button>
+                <button type="button" data-action="delete" class="btn-action">
+                    <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                </button>
+            </div>
+        </li>`;
+
+    tasksList.insertAdjacentHTML('beforeend',taskHTML);
+
+    taskInput.value="";
+    taskInput.focus();
+}
